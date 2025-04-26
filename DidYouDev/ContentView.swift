f15802c531c806881e43abdb632e9f9b4ad680f3
MaksGorobets <gorobets542@gmail.com>
2025-04-26 11:26:34 +0300

    Initial Commit

---
+++ DidYouDev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DidYouDev
+//
+//  Created by Maks Winters on 26.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DidYouDev/DidYouDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  DidYouDevApp.swift
+//  DidYouDev
+//
+//  Created by Maks Winters on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct DidYouDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
